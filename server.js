bf9c5f7afab4c74a16082124160e6dbb34d2e281
Jair0305 <chamaco_03@hotmail.es>
2024-11-22 01:25:44 -0600

    wip: testing deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use('/api', authRoutes);
 
 //changed port
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8181;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
