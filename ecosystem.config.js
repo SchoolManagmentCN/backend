ce3f24024f45d726c322937e8743b637e7f0ed33
Jair0305 <chamaco_03@hotmail.es>
2024-12-09 23:46:22 -0600

    sme changes

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+export default {
+    apps: [{
+        name: 'backend',
+        script: 'server.js',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: 'production',
+            PORT: 8080
+        }
+    }]
+}
